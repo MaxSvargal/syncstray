f936e4be156f91ca8dcac2eb25c49f63b58c4831
MaxSvargal <maxsvargal@gmail.com>
2016-11-15 15:01:45 +0300

    Fix production store configuration

--- app/store/configureStore.production.js
+++ app/store/configureStore.production.js
@@ -1,14 +1,17 @@
-// @flow
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { hashHistory } from 'react-router';
-import { routerMiddleware } from 'react-router-redux';
-import rootReducer from '../reducers';
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { hashHistory } from 'react-router'
+import { routerMiddleware } from 'react-router-redux'
+import rootReducer from 'reducers'
+import rootSaga from 'sagas'
 
-const router = routerMiddleware(hashHistory);
+const router = routerMiddleware(hashHistory)
+const sagaMiddleware = createSagaMiddleware()
 
-const enhancer = applyMiddleware(thunk, router);
+const enhancer = applyMiddleware(sagaMiddleware, router)
 
 export default function configureStore(initialState: Object) {
-  return createStore(rootReducer, initialState, enhancer);
+  const store = createStore(rootReducer, initialState, enhancer)
+  sagaMiddleware.run(rootSaga)
+  return store
 }
